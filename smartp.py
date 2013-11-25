48f7e457459991a439f2406ba75f22baf7a950c6
David González <nodearth@gmail.com>
2013-11-25 20:38:33 +0000

    first version working.

--- smartp.py
+++ smartp.py
@@ -1,14 +1,53 @@
 import sys
 import xml.etree.ElementTree as ET
 
+""" 
+	TODO: Check for property not found on update and delete
+		  Check for duplicated properties
+"""
+
 def update_property(dstfile, key, value):
+	data = ""
 	print "modify " + dstfile + " update " + " key " + " with value '" + value + "'"
 
+	with open(dstfile, "r+") as dstdescriptor:
+		for line in dstdescriptor:
+			if line.strip().startswith(key):
+				data = data + key + "=" + value
+			else:
+				data = data + line
+		dstdescriptor.seek(0)
+		dstdescriptor.write(data)
+		dstdescriptor.truncate()
+
 def delete_property(dstfile, key, value):
+	data = ""
 	print "modify " + dstfile + " delete " + " key " + " with value '" + value + "'"
 
+	with open(dstfile, "r+") as dstdescriptor:
+		for line in dstdescriptor:
+			if line.strip().startswith(key):
+				pass
+			else:
+				data = data + line
+
+		dstdescriptor.seek(0)
+		dstdescriptor.write(data)
+		dstdescriptor.truncate()
+
 def add_property(dstfile, key, value):
+	data = ""
 	print "modify " + dstfile + " add " + " key " + " with value '" + value + "'"
+
+	with open(dstfile, "r+") as dstdescriptor:
+		allfile = dstdescriptor.read()
+		allfile = allfile + "\n" + key + "=" + value
+		print allfile
+		data = allfile
+
+		dstdescriptor.seek(0)
+		dstdescriptor.write(data)
+		dstdescriptor.truncate()
 
 def apply_properties(srcfile, dstfile):
 	tree = ET.parse(srcfile)
@@ -22,17 +61,9 @@ def apply_properties(srcfile, dstfile):
 			delete_property(dstfile, property.find("key").text, property.find("value").text)
 
 def main(patchfile, dstpropertiesfile):
-	try:
 		with open(patchfile) as srcfile:
-			try:
-				with open(dstpropertiesfile) as dstfile:
-					apply_properties(patchfile, dstpropertiesfile)
-			except IOError:
-				print "destination file does not exists"
-				exit(-1)
-	except IOError:
-		print "destination file does not exists"
-		exit(-1)
+			with open(dstpropertiesfile) as dstfile:
+				apply_properties(patchfile, dstpropertiesfile)
 
 
 if __name__ == "__main__":
